919affd965e5afe33ae296248764b8b711c3244e
fzzl <464774994@qq.com>
2017-12-04 11:06:46 +0800

    代码测试------成功

---
+++ src/main/java/com/example/demo/demo/controller/Catcontroller.java
@@ -0,0 +1,41 @@
+package com.example.demo.demo.controller;
+
+import com.example.demo.demo.bean.Cat;
+import com.example.demo.demo.service.CatService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by wh1te on 2017/12/4.
+ */
+@RestController
+@RequestMapping("/cat")
+public class Catcontroller {
+
+    @Resource
+    private CatService catService;
+
+    @RequestMapping("/save")
+    public String save(){
+        Cat cat = new Cat();
+        cat.setCatName("jack");
+        cat.setCatAge(3);
+        catService.save(cat);
+        return "save";
+   }
+
+    @RequestMapping("/delete")
+    public String delete(){
+        catService.delete(1);
+        return "delete";
+    }
+
+    @RequestMapping("/getAll")
+    public Iterable<Cat> getAll(){
+        return catService.getAll();
+    }
+
+
+}
--- src/main/java/com/example/demo/demo/repository/CatRepository.java
+++ src/main/java/com/example/demo/demo/repository/CatRepository.java
@@ -4,10 +4,12 @@ import com.example.demo.demo.bean.Cat;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+
 /**
  * Created by wh1te on 2017/12/1.
  */
 @Repository
 public interface CatRepository extends CrudRepository <Cat,Integer>{
+
 
 }
